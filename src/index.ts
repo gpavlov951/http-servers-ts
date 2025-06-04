44566ff70d63efc97ccf4a51dbf8d8b8c8deca58
Georgi Pavlov <gpavlov951@gmail.com>
2025-06-04 12:40:33 +0300

    CH2. Routing - L1. Middleware

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.use(middlewareLogResponses);
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+function handlerReadiness(req: Request, res: Response) {
+  res.setHeader("Content-Type", "text/plain");
+  res.send("OK");
+}
+
+function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.on("finish", () => {
+    if (res.statusCode >= 400) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
+      );
+
+      return;
+    }
+
+    console.log(`[OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+  });
+
+  next();
+}

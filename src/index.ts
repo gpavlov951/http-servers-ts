c5f9e95b1621f9fb95cc7a8ee5fd6356d3294784
Georgi Pavlov <gpavlov951@gmail.com>
2025-06-04 14:54:27 +0300

    CH2. Routing - L2. API Config

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import { API_CONFIG } from "./config.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
 
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerMetrics);
+app.get("/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
@@ -20,6 +23,17 @@ app.listen(PORT, () => {
 function handlerReadiness(req: Request, res: Response) {
   res.setHeader("Content-Type", "text/plain");
   res.send("OK");
+}
+
+function handlerMetrics(req: Request, res: Response) {
+  res.setHeader("Content-Type", "text/plain");
+  res.send(`Hits: ${API_CONFIG.fileserverHits}`);
+}
+
+function handlerReset(req: Request, res: Response) {
+  API_CONFIG.fileserverHits = 0;
+  res.setHeader("Content-Type", "text/plain");
+  res.send("Reset successful");
 }
 
 function middlewareLogResponses(
@@ -39,5 +53,10 @@ function middlewareLogResponses(
     console.log(`[OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
   });
 
+  next();
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+  API_CONFIG.fileserverHits++;
   next();
 }
